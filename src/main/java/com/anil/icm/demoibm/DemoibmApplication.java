a7e46ca22ffa9f8200139a3e51ceaa1f202096dd
anilp02 <anilkumarp31@gmail.com>
2018-06-08 03:14:40 -0400

    inital

---
+++ src/main/java/com/anil/icm/demoibm/DemoibmApplication.java
@@ -0,0 +1,12 @@
+package com.anil.icm.demoibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoibmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoibmApplication.class, args);
+	}
+}
